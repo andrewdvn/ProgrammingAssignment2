29ad669009e72b78771019dbbcb33cdeb2d6cc69
ad <ad@adubuntudkt1204>
2014-12-21 23:05:14 +0000

    Added missing curly brace and amended some comments

--- cachematrix.R
+++ cachematrix.R
@@ -12,25 +12,26 @@
                              
 # makeCacheMatrix receives a matrix argument. This 
 # function then defines a further 4 functions:
-# 1) resetmatrix , which basically sets the matrix to that received 
+# 1) resetmatrix , which basically sets the original matrix to that received 
 # by this function and clears the inverse value i.e the result of any previous
 # call to solve to invert the matrix
 # 2) getmatr , simply returns the matrix
 # 3) setinverse, retains the value of the call to solve that inverts the matrix
-# 4) getinverse, returns the inverseval , which could be the cached, 
-# previously set value, or null 
+# 4) getinverse, returns the inverseval i.e inverted matrix, 
+# which could be the cached previously set value, or null 
 # These functions are returned by the function within a list, which 
 # effectively serves as an interface to the submitted matrix
 
-makeCacheMatrix <- function(x = matrix()) 
+makeCacheMatrix <- function(x = matrix())
+{ 
     inverseval <- NULL                 # clear the inverse matrix val
     resetmatrix <- function(matrixarg) # resets the original matrix to  
     {                                  # matrixarg and clears the inverted value
         x <<- matrixarg                # the scope <<- operator enables the 
         inverseval <<- NULL            # matrix and it's inverted form to be 
-    }                                  # held in a scope beyond the confines
-                                       # of the function, meaning it is 
-                                       # available to other session functions
+    }                                  # accessible in a parent session scope 
+                                       # i.e x is accessible from this function
+                                       # when it is called from cacheSolve.
     getmatr <- function() x            
     setinverse <- function(inverse) inverseval <<- inverse  # sets the inverted
                                                  # matrix using the scope
@@ -39,7 +40,9 @@ makeCacheMatrix <- function(x = matrix())
                                                 # caches the inverted matrix
                                               # so the process need not be 
                                               # repeated in this session
+
     getinverse <- function() inverseval       # returns the cached value
+
     list( resetmatrix = resetmatrix,       # return list of functions  
           getmatr = getmatr,               # effectively the interface to the 
           setinverse = setinverse,         # matrix and its inverted form
